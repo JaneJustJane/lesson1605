c8723bfec25901472ae7e00d0baa8b75417a70df
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-17 18:32:17 +0300

    sum of numbers from 1 to a

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число a");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+int b = 0;
+int sum=0;
+while (index <= a) 
+{
+    b = b+1;
+    sum = sum + b;
+    index++;
+}
+Console.WriteLine($"Сумма чисел от 1 до {a} равна {sum}");
