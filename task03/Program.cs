8e96f0ebb6252a130f2af916ea4c746a321da8e8
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-17 19:17:23 +0300

    a!

---
+++ task03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите число a");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+int comp=1;
+while (index <= a) 
+{
+    comp = comp * index;
+    index++;
+}
+Console.WriteLine($"Произведение чисел от 1 до {a} = {comp}");
