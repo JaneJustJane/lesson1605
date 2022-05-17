bbfd6da671c8d0e51e5fca72da98c76e51663407
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-17 18:04:45 +0300

    sq of the num 1-n

---
+++ task00/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число n");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+
+while (index <= n) 
+{
+    Console.WriteLine (index*index);
+    index++;
+}
