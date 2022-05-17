8b9caae19931744ae2be13315bf8a1175a50920e
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-17 18:53:37 +0300

    number of digits

---
+++ task02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число a");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int count = 1;
+while ((a/10)>0)
+{
+    count = count + 1;
+    a=a/10;
+}
+Console.WriteLine($"количество цифр в числе = {count}");
