1d8f44e20fb6f2e1f179a065b209b3ef07569f2c
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-18 12:13:51 +0300

    array

---
+++ task04/Program.cs
@@ -0,0 +1,23 @@
+﻿int [] array = new int [8];
+FillArray (array);
+void FillArray(int[] a)
+{
+    int Length = a.Length;
+    int index=0;
+    while (index<Length)
+    {
+        a[index]=new Random().Next(1,15);
+        index++;
+    }
+}
+PrintArray (array);
+void PrintArray(int[] b)
+{
+    int len1 = b.Length;
+    int index1=0;
+    while (index1<len1)
+    {
+        Console.WriteLine(b[index1]);
+        index1++;
+    }
+}
